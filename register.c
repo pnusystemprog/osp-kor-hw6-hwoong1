75bee00f1dc0854f0b514d8b551ff4460382ead3
jinyoung park <icarus012832@gmail.com>
2019-05-20 22:55:50 -0700

    Add a txt file and source codes

---
+++ register.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+#include"phone.h"
+extern int size;
+static int err=0;
+void registerPhoneData() {
+	char pw[] = "qwerty1234";
+	printf("password: ");
+
+do{
+	char a[30];
+	scanf("%s",a);
+	if(strcmp(a,pw)==0){
+		printf("New User Name: ");
+		scanf("%s", PhoneBook[size].Name);
+		printf("PhoneNumber: ");
+		scanf("%s", PhoneBook[size].PhoneNumber);
+		printf("Registered...");
+		size++;
+		break;
+	}
+	else{!
+		err++;
+		printf("wrong password!!(%d)",err);
+	if(err==3){
+		printf("You are not allowed to cccees PhoneBook.\n");
+		}
+	}
+}
+while(err != 3);  
+}
